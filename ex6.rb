9b49cce4f52f9421be3945857bdb4a3db907335f
Allyson Ooi <allysonooi28@gmail.com>
2016-04-11 11:20:51 +0800

    Understand the importance of Strings

---
+++ ex6.rb
@@ -0,0 +1,24 @@
+types_of_people = 10
+x = "There are #{types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = "Those who know #{binary} and those who #{do_not}!!!!!"
+
+puts x
+puts y
+
+puts "I said #{x}."
+puts "EH. But I also said #{y}!!>"
+
+hilarious = false
+joke_evaluation = "Isn't that joke funny?! #{hilarious}"
+
+puts joke_evaluation
+
+name = "Allyson"
+w = "This is the left side."
+e = "of a string with a right side!"
+z = "... #{do_not} be dumb, #{name}!"
+
+puts w + e
+puts z
