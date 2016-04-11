112f4db650f1a74c868775121205a8cc5d89c1af
Allyson Ooi <allysonooi28@gmail.com>
2016-04-11 11:17:10 +0800

    Learn the importance of Comments and Pound Characters

---
+++ ex2.rb
@@ -0,0 +1,25 @@
+puts "I will now count my fruits:"
+
+puts "Watermelons #{2 + 3 + 4 / 3}"
+puts "Strawberries #{2*5}"
+puts "Apples #{2*2*2*2/2}"
+
+puts "Now i will count the number of seeds:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "isit true that 5 * 2 > 6 + 45 ?"
+
+puts 5 * 2 > 6 + 45
+
+puts "What is 5/2? #{5/2}"
+puts "What is 6 - 9? #{6-9}"
+
+puts "Oh, that's why is is false."
+
+puts "How about somemore?"
+
+puts "Is it greater? #{10000*1 > 567/3}"
+puts "Is it greater or equal? #{10000*1 >= 567/3}"
+puts "Is it less or equal? #{10000*1 <= 567/3}"
+
