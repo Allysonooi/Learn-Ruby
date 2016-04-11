c96a9d8cc7f2987d04a19538e8d2d2d338afb2bd
Allyson Ooi <allysonooi28@gmail.com>
2016-04-11 11:19:18 +0800

    Learn the usage of Variables

---
+++ ex4.rb
@@ -0,0 +1,17 @@
+my_name = "Allyson"
+my_age = 19 # thats's not a lie 
+my_height = 153 #centimetres
+my_weight = 50 #kilograms
+my_eyes = "Brown"
+my_teeth = "white"
+my_hair = "Brown"
+
+puts "Lets talk about #{my_name}"
+puts "She is #{my_height} centimetres tall."
+puts "She is #{my_weight} kilograms heavy."
+puts "ACtually that isn't considered HEAVY.. I think?"
+puts "She has #{my_eyes} eyes and hair! Both of the same colour!"
+puts "Despite how her teeth is usually #{my_teeth}, coffee changes the colour."
+
+#This is tricky! Try to get it right bruh!
+puts "If I add #{my_age}, #{my_height} and #{my_weight}, I will get #{my_age + my_height + my_weight}"
