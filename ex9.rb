6d1db4430dd0e4ea8f008bc09ab35334446899ba
Allyson Ooi <allysonooi28@gmail.com>
2016-04-11 11:21:59 +0800

    Practice Printing

---
+++ ex9.rb
@@ -0,0 +1,15 @@
+#Here's some new strange stuff, rmb to type them exactly hoho!
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "These are the days: #{days}."
+puts "These are the months: #{months}."
+
+puts """
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we desire.
+Even 4 lines if we want, or 5, or 6.
+"""
+
