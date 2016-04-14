031b833739c20235c9f81257f1d2fe202c9d5681
Allyson Ooi <allysonooi28@gmail.com>
2016-04-15 02:43:31 +0800

    Learn how to access the elements of an array

---
+++ ex34.rb
@@ -0,0 +1,12 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+The animal at 1. Ruby
+The third (3rd) animal. Peacock
+The first (1st) animal. Bear
+The animal at 3. Kangaroo
+The fifth (5th) animal. Whale
+The animal at 2. Peacock
+The sixth (6th) animal. Platypus
+The animal at 4. Whale
+
+use: bear = animals[0] to check
